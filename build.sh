11647e326b6be8cd0b89bbd7fc7ec1c734ca2ae2
bob h <mail@bob.codes>
2022-03-30 15:51:10 +0100

    adding ca cert to Dockerfile
    ...also splitting build/deploy scripts

--- build.sh
+++ build.sh
@@ -1,19 +1,12 @@
 #! /bin/sh
 
 # base images
-for s in mcr.microsoft.com/dotnet/sdk:6.0 mcr.microsoft.com/dotnet/runtime:6.0 mariadb:latest
+for s in \
+	mariadb:latest \
+	mcr.microsoft.com/dotnet/runtime:6.0 \
+	mcr.microsoft.com/dotnet/sdk:6.0
 do
 	docker pull $s
-done
-
-# secrets
-for s in networktest_mariadb_password
-do
-  docker secret ls | tail --line +2 | grep $s >/dev/null
-
-  if [ $? -ne 0 ]; then
-    openssl rand -base64 201 | sed 's/[^0-9A-Za-z]//g' | sed -z 's/\n//g' | docker secret create $s -
-  fi
 done
 
 # build database image
@@ -21,8 +14,8 @@ docker build --tag eassbhhtgu/networktest-db:latest ./Database
 docker push eassbhhtgu/networktest-db:latest
 
 # build worker image
-docker build --tag eassbhhtgu/networktest:latest ./Worker
+docker build \
+	--secret id=ca_crt,src=${USERPROFILE}/.aspnet/https/ca.crt \
+	--tag eassbhhtgu/networktest:latest \
+	./Worker
 docker push eassbhhtgu/networktest:latest
-
-# docker deploy
-docker stack deploy --compose-file ./docker-compose.yml networktest
