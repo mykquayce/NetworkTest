b40248164332074f899f576094a9c5938b45a137
bob h <mail@bob.codes>
2022-08-01 15:51:39 +0100

    refresh script

---
+++ refresh.sh
@@ -0,0 +1,31 @@
+#! /bin/sh
+
+docker pull mariadb:latest
+docker pull mcr.microsoft.com/dotnet/runtime:6.0
+docker pull mcr.microsoft.com/dotnet/sdk:6.0
+docker pull eassbhhtgu/networktest:latest
+docker pull eassbhhtgu/networktest-db:latest
+
+
+
+base1=$(docker image inspect --format '{{.Created}}' mcr.microsoft.com/dotnet/runtime:6.0)
+base2=$(docker image inspect --format '{{.Created}}' mcr.microsoft.com/dotnet/sdk:6.0)
+base3=$(docker image inspect --format '{{.Created}}' mariadb:latest)
+image1=$(docker image inspect --format '{{.Created}}' eassbhhtgu/networktest:latest)
+image2=$(docker image inspect --format '{{.Created}}' eassbhhtgu/networktest-db:latest)
+
+
+
+if [[ $base1 > $image1 ]] || [[ $base2 > $image1 ]] || [[ $base3 > $image2 ]]; then
+
+	# build database image
+	docker build --tag eassbhhtgu/networktest-db:latest ./Database
+	docker push eassbhhtgu/networktest-db:latest
+
+	# build worker image
+	docker build \
+		--secret id=ca_crt,src=${USERPROFILE}/.aspnet/https/ca.crt \
+		--tag eassbhhtgu/networktest:latest \
+		./Worker
+	docker push eassbhhtgu/networktest:latest
+fi
